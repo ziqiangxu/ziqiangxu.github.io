7832df95fd5af177fa242ecf0bc94bd2954cfb7d
Daryl.Xu <ziqiang_xu@yeah.net>
2019-04-27 02:06:14 +0800

    new: generate html by vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+// 网站必要的配置文件
+module.exports = {
+    title: " ",
+    description: "description from config.js",
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            {
+                text: 'C',
+                items: [
+                    {text: 'CMake库文件添加', link: "/C/CMake之库文件添加.md"},
+                    {text: 'Qt获取信号发送者的地址', link: "/C/Qt获取信号发送者的地址.md"},
+                    {
+                        text: '使用XRecord实现Linux全局事件监听：扩展库安装篇',
+                        link: '/C/使用XRecord实现Linux全局事件监听：扩展库安装篇.md'
+                    }, 
+                    {
+                        text: '字符格式化',
+                        link: '/C/字符格式化.md'
+                    },
+                    {
+                        text: '给Qt初学者的建议',
+                        link: '/C/给Qt初学者的建议.md'
+                    }
+                ]
+            }
+        ],
+        // todo 自动化它
+        // 侧边栏，需要将路径写到这个列表里
+        // 注意： 目录下必须要有README.md， 【可能是因为目录最终显示的文字是来自于README.md的一级标题】
+        // 目录必须以斜杠结尾， 中文目录会导致无法生成跳转到下一页的箭头
+        sidebar: [
+            'C/',
+            'c1/',
+            'Docker/',
+            'ECMAScript/',
+            'Git/',
+            'Golang/',
+            'Java/',
+            'PHP/',
+            'Python/',
+            '个人日志/',
+            '操作系统/',
+            '数据结构/',
+            '积累/',
+            '网络安全/',
+            '计算机视觉/',
+            '软件工程/'
+        ]
+    }
+}
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,9 @@
+// 应用级别的配置文件
+export default ({
+    Vue,  // VuePress正在使用的Vue构造函数
+    options,  // 附加到根实例的一些选项
+    router,  // 当前应用的路由实例
+    siteData  // 站点元数据
+}) => {
+    // 做一些其他应用级别的优化
+}
