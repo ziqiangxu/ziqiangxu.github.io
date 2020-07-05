470128cd439f2344e49f60622eed1d4988a2802e
daryl <ziqiang_xu@qq.com>
2020-07-05 08:55:36 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,13 +18,13 @@ module.exports = {
         // 注意： 目录下必须要有README.md， 【可能是因为目录最终显示的文字是来自于README.md的一级标题】
         // 目录必须以斜杠结尾， 中文目录会导致无法生成跳转到下一页的箭头
         sidebar: funSets.getSubDirs('docs', {genReadme: false}),
+        // todo 按目录对侧边栏分组，然后文章要默认显示侧边栏（各级别header）
         // sidebar: 'auto',
         // sidebar: {
         // 空字符串表示README
         // "/computer-visual/": [{title: "计算机视觉", collapsable: false, children: ["", "Numpy一些易忘点", "人工智能研究流派", "图像的形态学操作"]}],
         // "/": funSets.getSubDirs('docs', {genReadme: false})
         // },
-        // displayAllHeaders: true,
         repo: "https://github.com/ziqiangxu/ziqiangxu.github.io",
         repoLabel: "查看源码",
         docsDir: "docs",
