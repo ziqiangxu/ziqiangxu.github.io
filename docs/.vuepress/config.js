347e6d937b24dd8e922b111db244d032601f0304
daryl <ziqiang_xu@qq.com>
2020-09-06 13:14:39 +0800

    config: update navigation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,46 @@
 "use strict";
 exports.__esModule = true;
-var funSets = require("./funSets");
-funSets.getSubDirs('docs', { genReadme: true });
+// var funSets = require("./funSets");
+// funSets.getSubDirs('docs', { genReadme: true });
 // 网站必要的配置文件
 module.exports = {
     base: "/blog/",
-    title: " ",
+    // title: "daryl",
     description: "description from config.js",
+    markdown: {
+        lineNumbers: true
+    },
     themeConfig: {
         // 导航栏
         nav: [
             { text: '主页', link: '/' },
-            // 必须以斜杠结尾才会路由到目录下的README
-            { text: '所有文章', link: '/accumulation/' },
+            // 以斜杠结尾会路由到目录下的README.md
+            { text: '未分类', link: '/accumulation/' },
+            { text: '编程语言', items: [
+                { text: 'Java', link: '/Java/' },
+                { text: 'Golang', link: '/Golang/' },
+                { text: 'Python', link: '/Python/' },
+                { text: 'C/C++', link: '/C/'}
+            ]},
+            { text: 'Web', items: [
+                { text: '网络安全', link: '/network-secure/' },
+                { text: 'ECMAScript', link: '/ECMAScript/'},
+                { text: 'Docker', link: '/Docker/' }
+            ]},
+            { text: '其它', items: [
+                { text: '软件工程', link: '/software-engineering/' },
+                { text: '数据结构', link: '/data-structure/' },
+                { text: '操作系统', link: '/OS/' },
+                { text: 'Git', link: '/Git/' },
+                { text: 'Qt', link: '/Qt/' },
+                { text: 'CV', link: '/computer-visual/'},
+            ]}
         ],
         // 侧边栏，需要将路径写到这个列表里
         // 注意： 目录下必须要有README.md， 【可能是因为目录最终显示的文字是来自于README.md的一级标题】
         // 目录必须以斜杠结尾， 中文目录会导致无法生成跳转到下一页的箭头
-        sidebar: funSets.getSubDirs('docs', {genReadme: false}),
+        // sidebar: funSets.getSubDirs('docs', {genReadme: false}),
+        // sidebar: ['docs/Java/', 'docs/Golang/'],
         // todo 按目录对侧边栏分组，然后文章要默认显示侧边栏（各级别header）
         // sidebar: 'auto',
         // sidebar: {
